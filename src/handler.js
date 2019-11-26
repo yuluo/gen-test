07efba559f076eda4e731cc8ee6411dcaa982c82
Yuanting Luo <yuanting.luo@viavisolutions.com>
2019-11-25 22:04:48 -0500

    Add openAPI url

--- src/handler.js
+++ src/handler.js
@@ -9,20 +9,8 @@ module.exports.parseSpec = async event => {
   const apiObject = await sp.dereference(event);
   const paths = apiObject.paths;
 
-  shell.rm("-rf", "generated/");
-  shell.mkdir("-p", "generated/node_modules");
-  shell.cp("-R", "src/test-libs/", "generated/node_modules");
-  shell.cp("-R", "config", "generated/node_modules");
-
-  //need to handle multiple servers
-  const testConfig = {
-    baseUrl: `http://localhost:8080${apiObject.servers[0].url}`
-  };
-
-  utils.writeFile(
-    `./generated/node_modules/config/test-config.json`,
-    JSON.stringify(testConfig, null, 2)
-  );
+  _setupWorkspace();
+  _createTestConfig(event, apiObject);
 
   Object.keys(paths).forEach(pathKey => {
     const path = paths[pathKey];
@@ -42,3 +30,33 @@ module.exports.parseSpec = async event => {
     });
   });
 };
+
+
+function _setupWorkspace() {
+  shell.rm("-rf", "generated/");
+  shell.mkdir("-p", "generated/node_modules");
+  shell.cp("-R", "src/test-libs/", "generated/node_modules");
+  shell.cp("-R", "config", "generated/node_modules");
+}
+
+function _createTestConfig(url, apiObject) {
+  const urlArray = url.split('/');
+  const rootUrl = urlArray.slice(0, urlArray.length - 1).join('/');
+  const baseUrls = apiObject.servers.map( server => {
+    var absolutePattern = /^https?:\/\//i;
+    if(absolutePattern.test(server.url)) {
+      return server.url;
+    } else {
+      return rootUrl + server.url;
+    }
+  })
+
+  const testConfig = {
+    baseUrl: baseUrls[0]
+  };
+
+  utils.writeFile(
+    `./generated/node_modules/config/test-config.json`,
+    JSON.stringify(testConfig, null, 2)
+  );
+}
