4df02b7a53ef75863c18ec00a20014b06f52a7c0
Yuanting Luo <yuanting.luo@rallyhealth.com>
2019-12-09 21:44:01 -0500

    Run prettier

--- src/libs/require-test-generator.spec.ts
+++ src/libs/require-test-generator.spec.ts
@@ -1,56 +1,69 @@
 import * as shell from "shelljs";
-import {mock, anything, when, instance, verify} from "ts-mockito";
-import {IRequireTestGenerator, IPayloadGenerator, IUtils} from "../interfaces";
-import {RequireTestGenerator} from "./require-test-generator";
+import { mock, anything, when, instance, verify } from "ts-mockito";
+import {
+  IRequireTestGenerator,
+  IPayloadGenerator,
+  IUtils
+} from "../interfaces";
+import { RequireTestGenerator } from "./require-test-generator";
 import * as petSchema from "../mocks/pet-schema.json";
 import * as petTemplate from "../mocks/pet-template.json";
 import { PayloadGenerator } from "./payload-generator";
 import { Utils } from "./utils";
 
 describe("RequireTestGenerator", () => {
-    let requireTestGenerator: IRequireTestGenerator;
-    let mockPayloadGenerator: IPayloadGenerator;
-    let mockUtils: IUtils;
-    let execSpy;
+  let requireTestGenerator: IRequireTestGenerator;
+  let mockPayloadGenerator: IPayloadGenerator;
+  let mockUtils: IUtils;
+  let execSpy;
 
-    beforeEach(() => {
-        execSpy = spyOn(shell, "exec");
-        mockPayloadGenerator = mock(PayloadGenerator);
-        mockUtils = mock(Utils);
+  beforeEach(() => {
+    execSpy = spyOn(shell, "exec");
+    mockPayloadGenerator = mock(PayloadGenerator);
+    mockUtils = mock(Utils);
 
-        when(mockPayloadGenerator.generatePayloadTemplate(anything())).thenReturn(petTemplate);
+    when(mockPayloadGenerator.generatePayloadTemplate(anything())).thenReturn(
+      petTemplate
+    );
 
-        requireTestGenerator = new RequireTestGenerator(instance(mockPayloadGenerator), instance(mockUtils));
-    });
-    
-    test("should run scaffold template", () => {
-        const expectedCmd = "hygen scaffold new --endpoint /pet --operation post --type require";
+    requireTestGenerator = new RequireTestGenerator(
+      instance(mockPayloadGenerator),
+      instance(mockUtils)
+    );
+  });
 
-        requireTestGenerator.generateTest("/pet", "post", petSchema);
-        expect(execSpy).toHaveBeenCalledWith(expectedCmd);
-    });
+  test("should run scaffold template", () => {
+    const expectedCmd =
+      "hygen scaffold new --endpoint /pet --operation post --type require";
 
-    test("should run positive test template", () => {
-        const expectedCmd = "hygen require-test new --endpoint /pet --operation post --name positive --datafile payload-1.json --codes successCodes";
+    requireTestGenerator.generateTest("/pet", "post", petSchema);
+    expect(execSpy).toHaveBeenCalledWith(expectedCmd);
+  });
 
-        requireTestGenerator.generateTest("/pet", "post", petSchema);
-        expect(execSpy).toHaveBeenCalledWith(expectedCmd);
-    });
+  test("should run positive test template", () => {
+    const expectedCmd =
+      "hygen require-test new --endpoint /pet --operation post --name positive --datafile payload-1.json --codes successCodes";
 
+    requireTestGenerator.generateTest("/pet", "post", petSchema);
+    expect(execSpy).toHaveBeenCalledWith(expectedCmd);
+  });
 
-    test("should run positive test template", () => {
-        const expectedCmd = "hygen require-test new --endpoint /pet --operation post --name \"negative-0 missing name\" --datafile payload-2.json --codes failCodes";
+  test("should run positive test template", () => {
+    const expectedCmd =
+      'hygen require-test new --endpoint /pet --operation post --name "negative-0 missing name" --datafile payload-2.json --codes failCodes';
 
-        requireTestGenerator.generateTest("/pet", "post", petSchema);
-        expect(execSpy).toHaveBeenCalledWith(expectedCmd);
-    });
+    requireTestGenerator.generateTest("/pet", "post", petSchema);
+    expect(execSpy).toHaveBeenCalledWith(expectedCmd);
+  });
 
-    test("should write positive payload", () => {
-        const expectedPath = "./generated//pet/post/require-test/payload-1.json";
-        const expectedPayloadString = JSON.stringify(petTemplate, null, 2);
+  test("should write positive payload", () => {
+    const expectedPath = "./generated//pet/post/require-test/payload-1.json";
+    const expectedPayloadString = JSON.stringify(petTemplate, null, 2);
 
-        requireTestGenerator.generateTest("/pet", "post", petSchema);
+    requireTestGenerator.generateTest("/pet", "post", petSchema);
 
-        verify(mockUtils.writeFileUtil(expectedPath, expectedPayloadString)).called();
-    });
+    verify(
+      mockUtils.writeFileUtil(expectedPath, expectedPayloadString)
+    ).called();
+  });
 });
